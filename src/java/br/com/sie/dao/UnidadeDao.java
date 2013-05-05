9b1e98d54e9ceb3206905e2b9ef1e9c7e71a3f54
Diego <diego.litus@gmail.com>
2013-05-05 19:11:37 -0300

    Adiciona a classe UnidadeDAO ao pacote br.com.sie.dao

---
+++ src/java/br/com/sie/dao/UnidadeDao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sie.dao;
+
+import br.com.sie.beans.Unidade;
+
+/**
+ *
+ * @author Diego
+ */
+public class UnidadeDao extends GenericDao<Unidade>{
+    /**
+     *
+     */
+    public UnidadeDao(){
+        super(Unidade.class);
+    }
+    
+}
