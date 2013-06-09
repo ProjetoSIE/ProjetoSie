c305f26b2bc0c5442d6863d15f724fe417d2bb05
Marcos Costa <marcosta1993@hotmail.com>
2013-06-09 13:54:00 -0300

    Unidade Test

---
+++ test/br/com/sie/beans/UnidadeTest.java
@@ -0,0 +1,498 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sie.beans;
+
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Marcos Costa
+ */
+public class UnidadeTest {
+    
+    public UnidadeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getId method, of class Unidade.
+     */
+    @Test
+    public void testGetId() {
+        System.out.println("getId");
+        Unidade instance = new Unidade();
+        Long expResult = null;
+        Long result = instance.getId();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setId method, of class Unidade.
+     */
+    @Test
+    public void testSetId() {
+        System.out.println("setId");
+        Long id = null;
+        Unidade instance = new Unidade();
+        instance.setId(id);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getRazaoSocial method, of class Unidade.
+     */
+    @Test
+    public void testGetRazaoSocial() {
+        System.out.println("getRazaoSocial");
+        Unidade instance = new Unidade();
+        String expResult = "";
+        String result = instance.getRazaoSocial();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setRazaoSocial method, of class Unidade.
+     */
+    @Test
+    public void testSetRazaoSocial() {
+        System.out.println("setRazaoSocial");
+        String razaoSocial = "";
+        Unidade instance = new Unidade();
+        instance.setRazaoSocial(razaoSocial);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getNomeFantasia method, of class Unidade.
+     */
+    @Test
+    public void testGetNomeFantasia() {
+        System.out.println("getNomeFantasia");
+        Unidade instance = new Unidade();
+        String expResult = "";
+        String result = instance.getNomeFantasia();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setNomeFantasia method, of class Unidade.
+     */
+    @Test
+    public void testSetNomeFantasia() {
+        System.out.println("setNomeFantasia");
+        String nomeFantasia = "";
+        Unidade instance = new Unidade();
+        instance.setNomeFantasia(nomeFantasia);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCnpj method, of class Unidade.
+     */
+    @Test
+    public void testGetCnpj() {
+        System.out.println("getCnpj");
+        Unidade instance = new Unidade();
+        int expResult = 0;
+        int result = instance.getCnpj();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCnpj method, of class Unidade.
+     */
+    @Test
+    public void testSetCnpj() {
+        System.out.println("setCnpj");
+        int cnpj = 0;
+        Unidade instance = new Unidade();
+        instance.setCnpj(cnpj);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAbertura method, of class Unidade.
+     */
+    @Test
+    public void testGetAbertura() {
+        System.out.println("getAbertura");
+        Unidade instance = new Unidade();
+        Date expResult = null;
+        Date result = instance.getAbertura();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setAbertura method, of class Unidade.
+     */
+    @Test
+    public void testSetAbertura() {
+        System.out.println("setAbertura");
+        Date abertura = null;
+        Unidade instance = new Unidade();
+        instance.setAbertura(abertura);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getInscricaoEstadual method, of class Unidade.
+     */
+    @Test
+    public void testGetInscricaoEstadual() {
+        System.out.println("getInscricaoEstadual");
+        Unidade instance = new Unidade();
+        int expResult = 0;
+        int result = instance.getInscricaoEstadual();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setInscricaoEstadual method, of class Unidade.
+     */
+    @Test
+    public void testSetInscricaoEstadual() {
+        System.out.println("setInscricaoEstadual");
+        int inscricaoEstadual = 0;
+        Unidade instance = new Unidade();
+        instance.setInscricaoEstadual(inscricaoEstadual);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getInscricaoMunicipal method, of class Unidade.
+     */
+    @Test
+    public void testGetInscricaoMunicipal() {
+        System.out.println("getInscricaoMunicipal");
+        Unidade instance = new Unidade();
+        int expResult = 0;
+        int result = instance.getInscricaoMunicipal();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setInscricaoMunicipal method, of class Unidade.
+     */
+    @Test
+    public void testSetInscricaoMunicipal() {
+        System.out.println("setInscricaoMunicipal");
+        int inscricaoMunicipal = 0;
+        Unidade instance = new Unidade();
+        instance.setInscricaoMunicipal(inscricaoMunicipal);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getEmail method, of class Unidade.
+     */
+    @Test
+    public void testGetEmail() {
+        System.out.println("getEmail");
+        Unidade instance = new Unidade();
+        String expResult = "";
+        String result = instance.getEmail();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setEmail method, of class Unidade.
+     */
+    @Test
+    public void testSetEmail() {
+        System.out.println("setEmail");
+        String email = "";
+        Unidade instance = new Unidade();
+        instance.setEmail(email);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSite method, of class Unidade.
+     */
+    @Test
+    public void testGetSite() {
+        System.out.println("getSite");
+        Unidade instance = new Unidade();
+        String expResult = "";
+        String result = instance.getSite();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSite method, of class Unidade.
+     */
+    @Test
+    public void testSetSite() {
+        System.out.println("setSite");
+        String site = "";
+        Unidade instance = new Unidade();
+        instance.setSite(site);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getObservacoe method, of class Unidade.
+     */
+    @Test
+    public void testGetObservacoe() {
+        System.out.println("getObservacoe");
+        Unidade instance = new Unidade();
+        String expResult = "";
+        String result = instance.getObservacoe();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setObservacoe method, of class Unidade.
+     */
+    @Test
+    public void testSetObservacoe() {
+        System.out.println("setObservacoe");
+        String observacoe = "";
+        Unidade instance = new Unidade();
+        instance.setObservacoe(observacoe);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getLogradouro method, of class Unidade.
+     */
+    @Test
+    public void testGetLogradouro() {
+        System.out.println("getLogradouro");
+        Unidade instance = new Unidade();
+        String expResult = "";
+        String result = instance.getLogradouro();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setLogradouro method, of class Unidade.
+     */
+    @Test
+    public void testSetLogradouro() {
+        System.out.println("setLogradouro");
+        String logradouro = "";
+        Unidade instance = new Unidade();
+        instance.setLogradouro(logradouro);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getNumero method, of class Unidade.
+     */
+    @Test
+    public void testGetNumero() {
+        System.out.println("getNumero");
+        Unidade instance = new Unidade();
+        String expResult = "";
+        String result = instance.getNumero();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setNumero method, of class Unidade.
+     */
+    @Test
+    public void testSetNumero() {
+        System.out.println("setNumero");
+        String numero = "";
+        Unidade instance = new Unidade();
+        instance.setNumero(numero);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getComplemento method, of class Unidade.
+     */
+    @Test
+    public void testGetComplemento() {
+        System.out.println("getComplemento");
+        Unidade instance = new Unidade();
+        String expResult = "";
+        String result = instance.getComplemento();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setComplemento method, of class Unidade.
+     */
+    @Test
+    public void testSetComplemento() {
+        System.out.println("setComplemento");
+        String complemento = "";
+        Unidade instance = new Unidade();
+        instance.setComplemento(complemento);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getBairro method, of class Unidade.
+     */
+    @Test
+    public void testGetBairro() {
+        System.out.println("getBairro");
+        Unidade instance = new Unidade();
+        String expResult = "";
+        String result = instance.getBairro();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setBairro method, of class Unidade.
+     */
+    @Test
+    public void testSetBairro() {
+        System.out.println("setBairro");
+        String bairro = "";
+        Unidade instance = new Unidade();
+        instance.setBairro(bairro);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCep method, of class Unidade.
+     */
+    @Test
+    public void testGetCep() {
+        System.out.println("getCep");
+        Unidade instance = new Unidade();
+        String expResult = null;
+        String result = instance.getCep();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCep method, of class Unidade.
+     */
+    @Test
+    public void testSetCep() {
+        System.out.println("setCep");
+        String cep = null;
+        Unidade instance = new Unidade();
+        instance.setCep(cep);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCidade method, of class Unidade.
+     */
+    @Test
+    public void testGetCidade() {
+        System.out.println("getCidade");
+        Unidade instance = new Unidade();
+        String expResult = "";
+        String result = instance.getCidade();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCidade method, of class Unidade.
+     */
+    @Test
+    public void testSetCidade() {
+        System.out.println("setCidade");
+        String cidade = "";
+        Unidade instance = new Unidade();
+        instance.setCidade(cidade);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getEstado method, of class Unidade.
+     */
+    @Test
+    public void testGetEstado() {
+        System.out.println("getEstado");
+        Unidade instance = new Unidade();
+        String expResult = "";
+        String result = instance.getEstado();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setEstado method, of class Unidade.
+     */
+    @Test
+    public void testSetEstado() {
+        System.out.println("setEstado");
+        String estado = "";
+        Unidade instance = new Unidade();
+        instance.setEstado(estado);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
